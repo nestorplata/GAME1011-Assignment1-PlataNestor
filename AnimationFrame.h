75b78f588271e5c47f76f635f0d2f5184c817b68
nestorplata <Nestor.PlataGomez@georgebrown.ca>
2022-02-13 00:51:04 -0500

    Implementation of Animation Frame

--- AnimationFrame.h
+++ AnimationFrame.h
@@ -1,1 +1,62 @@
 #pragma once
+
+#include <iostream>
+#include <fstream>
+
+class AnimationFrame {
+public:
+
+	void setfile(std::string File)
+	{
+		inFile.open(File);
+		if (!inFile) {
+			std::cout << "Unable to open file datafile.txt";
+		}
+		else
+		{
+			setframe1();
+			setframe2();
+			setframe3();
+		}
+	}
+
+	void setframe1(){
+		inFile.get(frame1[0], 11);
+		inFile.get(frame1[2], 11);
+		inFile.get(frame1[4], 11);
+	}
+
+	void setframe2() {
+		inFile.get(frame2[1], 11);
+		inFile.get(frame2[3], 11);
+		inFile.get(frame2[6], 11);
+	}
+	void setframe3() {
+		inFile.get(frame3[2], 11);
+		inFile.get(frame3[5], 11);
+		inFile.get(frame3[8], 11);
+	}
+
+	std::string getframe1(int i) {
+		return frame1[i];
+	}
+
+	std::string getframe2(int i) {
+		return frame2[i];
+	}
+
+	std::string getframe3(int i) {
+		return frame3[i];
+	}
+
+	void close() {
+		inFile.close();
+	}
+
+private:
+	std::ifstream inFile;
+	char frame1[5][10];
+	char frame2[7][10];
+	char frame3[9][10];
+
+};
--- Slug.h
+++ Slug.h
@@ -1,78 +1,46 @@
 #pragma once
-#include <iostream>
-#include<fstream>
-
-class Slug {
+//#include <iostream>
+//#include<fstream>
+#include "AnimationFrame.h"
+class Slug{
 public:
 	Slug() {
 		//setInitialText();
-		setfile("Slug.txt");
-	}
-
-	void setfile(std::string File)
-	{
-		inFile.open(File);
-		if (!inFile) {
-			std::cout << "Unable to open file datafile.txt";
-		}
-		else
-		{
-			setText();
-		}
-	}
-
-	void setText()
-	{
-		inFile.get(frame1[0], 11);
-		inFile.get(frame1[2], 11);
-		inFile.get(frame1[4], 11);
-
-		inFile.get(frame2[1], 11);
-		inFile.get(frame2[3], 11);
-		inFile.get(frame2[6], 11);
-
-		inFile.get(frame3[2], 11);
-		inFile.get(frame3[5], 11);
-		inFile.get(frame3[8], 11);
-
-	}
-
-	void close() {
-		inFile.close();
+		Frames.setfile("Slug.txt");
 	}
 
 	void Update() {
 		while (isUpdating) {
 			system("CLS");
-			std::cout << spaces << frame1[0] << std::endl;
-			std::cout << spaces << frame1[2] << std::endl;
-			std::cout << spaces << frame1[4] << std::endl;
+			std::cout << spaces << Frames.getframe1(0) << std::endl;
+			std::cout << spaces << Frames.getframe1(2) << std::endl;
+			std::cout << spaces << Frames.getframe1(4) << std::endl;
 			Sleep(milli_seconds);
 
 			system("CLS");
-			std::cout << spaces << "\t" << frame3[2] << std::endl;
-			std::cout << spaces << "\t" << frame3[5] << std::endl;
-			std::cout << spaces << "\t" << frame3[8] << std::endl;
+			std::cout << spaces << "\t" << Frames.getframe3(2) << std::endl;
+			std::cout << spaces << "\t" << Frames.getframe3(5) << std::endl;
+			std::cout << spaces << "\t" << Frames.getframe3(8) << std::endl;
 			Sleep(milli_seconds);
 
 			system("CLS");
-			std::cout << spaces << "\t\t" << frame2[1] << std::endl;
-			std::cout << spaces << "\t\t" << frame2[3] << std::endl;
-			std::cout << spaces << "\t\t" << frame2[6] << std::endl;
+			std::cout << spaces << "\t\t" << Frames.getframe2(1) << std::endl;
+			std::cout << spaces << "\t\t" << Frames.getframe2(3) << std::endl;
+			std::cout << spaces << "\t\t" << Frames.getframe2(6) << std::endl;
 			Sleep(milli_seconds);
 
 			system("CLS");
-			std::cout << spaces << "\t\t\t" << frame3[2] << std::endl;
-			std::cout << spaces << "\t\t\t" << frame3[5] << std::endl;
-			std::cout << spaces << "\t\t\t" << frame3[8] << std::endl;
+			std::cout << spaces << "\t\t\t" << Frames.getframe3(2) << std::endl;
+			std::cout << spaces << "\t\t\t" << Frames.getframe3(5) << std::endl;
+			std::cout << spaces << "\t\t\t" << Frames.getframe3(8) << std::endl;
 			Sleep(milli_seconds);
 
 			if (i == 2)
 			{
 				system("CLS");
-				std::cout << spaces << "\t\t\t\t" << frame1[0] << std::endl;
-				std::cout << spaces << "\t\t\t\t" << frame1[2] << std::endl;
-				std::cout << spaces << "\t\t\t\t" << frame1[4] << std::endl;
+				std::cout << spaces << "\t\t\t\t" << Frames.getframe1(0) << std::endl;
+				std::cout << spaces << "\t\t\t\t" << Frames.getframe1(2) << std::endl;
+				std::cout << spaces << "\t\t\t\t" << Frames.getframe1(4) << std::endl;
 				Sleep(milli_seconds);
 				system("CLS");
 				isUpdating = false;
@@ -94,13 +62,12 @@ public:
 	}
 
 private:
-	std::ifstream inFile;
+
+	AnimationFrame Frames;
 	std::string spaces;
-	char frame1[5][10];
-	char frame2[7][10];
-	char frame3[9][10];
 	int i = 0;
-	bool isUpdating =true;
 	const int milli_seconds = 500;
+	bool isUpdating =true;
+
 
 };
